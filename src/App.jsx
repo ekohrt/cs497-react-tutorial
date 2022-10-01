5ea17339bb5a698ff318d68fd8e07e8ce1073089
Ethan Kohrt <e.a.kohrt@gmail.com>
2022-10-01 13:23:27 -0500

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = {
+  title: "CS Courses for 2018-2019"
+}
+
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -38,6 +42,7 @@ const App = () => {
           </a>
         </p>
       </header>
+      <h1>{schedule.title}</h1>
     </div>
   );
 };
